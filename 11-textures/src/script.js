4d5dcfb07ea09ae301a835392fdd4a993142152b
lwood54 <lwood3499@gmail.com>
2021-08-27 21:17:53 -0500

    through 11

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -2,17 +2,77 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import imageSource from "../static/textures/door/color.jpg";
-console.log("imageSource", imageSource);
 
 // TEXTURES
-const image = new Image();
-// convert image to a texture
-const texture = new THREE.Texture(image);
-image.onload = () => {
-  // update texture when image is loaded
-  texture.needsUpdate = true;
+//// BETTER WAY ////
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => {
+  console.log("onStart");
 };
-image.src = "/textures/door/color.jpg";
+loadingManager.onLoad = () => {
+  console.log("onLoad");
+};
+loadingManager.onProgress = () => {
+  console.log("onProgress");
+};
+loadingManager.onError = () => {
+  console.log("onError");
+};
+const textureLoader = new THREE.TextureLoader(loadingManager); // One TextureLoader can load multiple textures
+// const colorTexture = textureLoader.load("/textures/door/color.jpg");
+const colorTexture = textureLoader.load("/textures/minecraft.png");
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+// colorTexture.repeat.x = 2;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.RepeatWrapping;
+// colorTexture.wrapT = THREE.RepeatWrapping;
+
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.75;
+
+// colorTexture.rotation = Math.PI * 0.25;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+// if possible, preventing mipmap generation is more performant,
+// but only use if benefits of mipmapping are not needed
+// ex: if using .NearestFilter inside the minFilter, you can set generateMipmaps to false
+// NOTE: use power of 2 resolutions because THREE js will try to resize for it to work and it will be more expensive
+colorTexture.generateMipmaps = false;
+colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.magFilter = THREE.NearestFilter;
+
+// const texture = textureLoader.load(
+// "/textures/door/color.jpg",
+// callbacks available for extra info
+// () => {
+//   console.log("load");
+// },
+// () => {
+//   console.log("progress");
+// },
+// () => {
+//   console.log("error");
+// }
+// );
+//// BETTER WAY ////
+//// MANUAL WAY ////
+// const image = new Image();
+// // convert image to a texture
+// const texture = new THREE.Texture(image);
+// image.onload = () => {
+//   // update texture when image is loaded
+//   texture.needsUpdate = true;
+// };
+// image.src = "/textures/door/color.jpg";
+//// MANUAL WAY ////
 
 /**
  * Base
@@ -27,7 +87,7 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ map: texture });
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
